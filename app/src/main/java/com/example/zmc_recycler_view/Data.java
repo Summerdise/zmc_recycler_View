13d2190a68e3145e86ef19d6cfd6a479a25d9baf
Summerdise <350464517@qq.com>
2020-08-17 16:35:27 +0800

    [zmc] feat: add recycler activity layout

---
+++ app/src/main/java/com/example/zmc_recycler_view/Data.java
@@ -0,0 +1,14 @@
+package com.example.zmc_recycler_view;
+
+public class Data {
+    public String title;
+    public String description;
+    public int number;
+
+    public Data(String title, String description, int number) {
+        this.title = title;
+        this.description = description;
+        this.number = number;
+    }
+
+}
