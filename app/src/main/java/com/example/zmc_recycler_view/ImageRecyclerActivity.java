fdb719ff35a21e03de0812b21c215305f83eab06
Summerdise <350464517@qq.com>
2020-08-18 10:59:34 +0800

    [zmc] feat: add files for third task

---
+++ app/src/main/java/com/example/zmc_recycler_view/ImageRecyclerActivity.java
@@ -0,0 +1,14 @@
+package com.example.zmc_recycler_view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ImageRecyclerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_recycler);
+    }
+}
