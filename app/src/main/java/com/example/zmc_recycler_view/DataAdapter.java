542e5d0b53ea5719c34fb9682bbc6e31e23a643c
Summerdise <350464517@qq.com>
2020-08-18 16:53:49 +0800

    [zmc] fix: fix all text color to black

--- app/src/main/java/com/example/zmc_recycler_view/DataAdapter.java
+++ app/src/main/java/com/example/zmc_recycler_view/DataAdapter.java
@@ -1,7 +1,6 @@
 package com.example.zmc_recycler_view;
 
 import android.content.Context;
-import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
