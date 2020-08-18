2e0202675c4335be58043d94be221bd9c586efbb
Summerdise <350464517@qq.com>
2020-08-18 10:53:40 +0800

    [zmc] feat: add two view to showing differently

--- app/src/main/java/com/example/zmc_recycler_view/HeaderRecyclerActivity.java
+++ app/src/main/java/com/example/zmc_recycler_view/HeaderRecyclerActivity.java
@@ -1,18 +1,15 @@
 package com.example.zmc_recycler_view;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
-import android.widget.LinearLayout;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class RecyclerActivity extends AppCompatActivity {
-
+public class HeaderRecyclerActivity extends AppCompatActivity {
     private List<Data> dataList = new ArrayList<>();
 
     @Override
@@ -28,6 +25,7 @@ public class RecyclerActivity extends AppCompatActivity {
     }
 
     private void initFruits(int size) {
+        dataList.add(new Data(1, "This is header", null, 0));
         for (int i = 0; i < size; i++) {
             int number = i + 1;
             dataList.add(new Data(0, String.format("title%d", number), String.format("desp%d", number), number));
--- app/src/main/java/com/example/zmc_recycler_view/MainActivity.java
+++ app/src/main/java/com/example/zmc_recycler_view/MainActivity.java
@@ -13,11 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView recyclerViewEntry = findViewById(R.id.entry);
+
+        TextView recyclerViewEntry = findViewById(R.id.recycler_entry);
         recyclerViewEntry.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this,RecyclerActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        TextView headerRecyclerViewEntry = findViewById(R.id.header_recycler_entry);
+        headerRecyclerViewEntry.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,HeaderRecyclerActivity.class);
                 startActivity(intent);
             }
         });
